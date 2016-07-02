2c41bfc8fdb3da6f1f58e4aef04cf46f8951e6f0
chennan625 <chennan625@gmail.com>
2016-07-01 23:48:50 -0500

    Chapter 7 assignments

---
+++ Chapter7/assignments7.3.py
@@ -0,0 +1,21 @@
+'''
+Modify the program that prompts the user for the file name so that it prints a funny message when the user
+types in the exact file name "na na boo boo".
+'''
+fname = raw_input('Enter the file name:')
+try:
+    fhand = open(fname)
+except:
+    if fname == "na na boo boo":
+        print "NA NA BOO BOO TO YOU - You have been punk'd!"
+        exit()
+    else:
+        print 'File cannot be opened:', fname
+        exit()
+
+count = 0
+for line in fhand:
+    if line.startswith('Subject:'):
+        count = count + 1
+print 'There were', count, 'subject lines in', fname
+
